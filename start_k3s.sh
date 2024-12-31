b64dbe809913d316ba77ff3ecd78ded1535886c8
Jason <sysbender@gmail.com>
2024-12-31 16:56:29 +0000

    fix frp

--- start_k3s.sh
+++ start_k3s.sh
@@ -10,19 +10,33 @@
 
 
 echo "Installing K3s..."
-curl -sfL https://get.k3s.io | sh -
+#curl -sfL https://get.k3s.io | sh -
 
 echo "Configuring kubectl for the current user..."
 USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
 USER_NAME=${SUDO_USER:-$USER}
 
+echo "userhome = $USER_HOME"
+echo "username = $USER_NAME"
+
 # Set up kubeconfig for the user
 mkdir -p "$USER_HOME/.kube"
-cp /etc/rancher/k3s/k3s.yaml "$USER_HOME/.kube/config"
-chown -R "$USER_NAME:$USER_NAME" "$USER_HOME/.kube"
-chmod 600 "$USER_HOME/.kube/config"
+sudo cp /etc/rancher/k3s/k3s.yaml "$USER_HOME/.kube/config"
+sudo chown -R "$USER_NAME:$USER_NAME" "$USER_HOME/.kube"
+sudo chmod 600 "$USER_HOME/.kube/config"
 
 echo "K3s configuration has been copied to $USER_HOME/.kube/config"
 
-export KUBEVERSION=$(kubectl version --short | awk -F ': ' 'NR==1 {print $2}')
-echo 'K3s cluster started with version $KUBEVERSION'
+# Define the line to add
+LINE="export KUBECONFIG=$USER_HOME/.kube/config"
+
+# Check if the line already exists in .bashrc
+if ! grep -Fxq "$LINE" "$USER_HOME/.bashrc"; then
+  echo "$LINE" >> "$USER_HOME/.bashrc"
+  echo "Added KUBECONFIG export to $USER_HOME/.bashrc"
+else
+  echo "KUBECONFIG export already exists in $USER_HOME/.bashrc"
+fi
+
+# export KUBEVERSION=$(kubectl version --short | awk -F ': ' 'NR==1 {print $2}')
+# echo 'K3s cluster started with version $KUBEVERSION'
